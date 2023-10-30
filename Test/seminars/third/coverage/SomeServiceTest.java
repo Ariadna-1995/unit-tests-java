bd991e4e522f957e60df74a287053593de6c1520
ARIADNA <rariadna@inbox.ru>
2023-10-30 19:20:50 +0300

    Changes added

--- Test/seminars/third/coverage/SomeServiceTest.java
+++ Test/seminars/third/coverage/SomeServiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class SomeServiceTest {
-    // 3.1.**************************************************************************
+
     SomeService service;
 
     @BeforeEach
